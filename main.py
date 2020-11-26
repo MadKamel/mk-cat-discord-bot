dc92feee2302fd193ff5ba0d5f1606bb5b5b9421
Erica Rustrum <rustrumek@gmail.com>
2020-11-26 00:29:07 +0000

    Added basic code.

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-import discord
+import discord, os
+os.system('clear')
+
+intents = discord.Intents.all()
+token = os.getenv('token')
+client = discord.Client(intents=intents)
+
+
+
+@client.event
+async def on_message(message):
+    if client.user.mention in message.content.split():
+        await message.channel.send('meow')
